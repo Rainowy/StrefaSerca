e5378f8109177469a6c1172c022459e250929003
Tomek <tomasz.czarny.czarnecki@gmail.com>
2020-05-17 16:55:52 +0200

    Initial commit

---
+++ src/main/java/pl/strefaserca/portal/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.strefaserca.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+//    @GetMapping("/get")
+//    public ModelAndView index(){
+//        return new ModelAndView("index");
+//    }
+
+}
