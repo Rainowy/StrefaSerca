4411417f01c174c5bd1fb52526ab5d7a8da5e1d4
Tomek <tomasz.czarny.czarnecki@gmail.com>
2020-05-19 09:27:56 +0200

    All needed pages added I suppose

--- src/main/java/pl/strefaserca/portal/controller/HomeController.java
+++ src/main/java/pl/strefaserca/portal/controller/HomeController.java
@@ -7,9 +7,38 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-//    @GetMapping("/get")
-//    public ModelAndView index(){
-//        return new ModelAndView("index");
-//    }
+    @GetMapping("/index")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
 
+    @GetMapping("/certificates")
+    public ModelAndView certificates(){
+        return new ModelAndView("certificates");
+    }
+
+    @GetMapping("/services")
+    public ModelAndView services(){
+        return new ModelAndView("services");
+    }
+
+    @GetMapping("/service-detail")
+    public ModelAndView serviceDetail(){
+        return new ModelAndView("service-detail");
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView contact(){
+        return new ModelAndView("contact");
+    }
+
+    @GetMapping("/blog")
+    public ModelAndView blog(){
+        return new ModelAndView("blog");
+    }
+
+    @GetMapping("/testimonials")
+    public ModelAndView testimonials(){
+        return new ModelAndView("testimonials");
+    }
 }
