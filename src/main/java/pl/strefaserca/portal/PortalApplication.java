7c442a795ee7849c555f496ba81cc04948ea07bb
tomek <tomasz.czarny.czarnecki@gmail.com>
2022-10-27 13:19:24 +0200

    redirect connector changes - scheme to https, setsecure to true, removed redirectport 8443

--- src/main/java/pl/strefaserca/portal/PortalApplication.java
+++ src/main/java/pl/strefaserca/portal/PortalApplication.java
@@ -37,10 +37,10 @@ public class PortalApplication {
 
 	private Connector redirectConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
+		connector.setScheme("https");
 		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
+		connector.setSecure(true);
+//		connector.setRedirectPort(8443);
 		return connector;
 	}
 }
