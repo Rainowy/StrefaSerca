7a7c8fdf44d1acf8ddbe0dd074cb889074b85132
Tomek <tomasz.czarny.czarnecki@gmail.com>
2023-12-28 17:04:03 +0100

    JDK 17 installed. Amazon services removed to gain space. Config files addes to 'assume unchanged'

--- target.bak/classes/static/js/firstVisitModal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    //Show modal on first visit and save to localStorage
-    let ls = localStorage.getItem('strefa.visited');
-    if (ls == null) {
-        $('#cookieModal').modal('show');
-        localStorage.setItem('strefa.visited', 1)
-    }
-});
--- target.bak/classes/static/js/sticky-header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  	"use strict";
-	 if( $( window ).width() >= "768" ) {
-		$(".header").sticky({topSpacing:0});	
-    }
-
-  });
-  
